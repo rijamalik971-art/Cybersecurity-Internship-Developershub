94a63250d16754dc668304873e11868a7e32a37c
rijamalik971-art <rijamalik971@gmail.com>
2026-06-26 14:58:23 +0500

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+        })
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'security.log' })
+    ]
+});
+
+logger.info('Logger initialized successfully');
+
+module.exports = logger;
